c94612271bfeac4ea09bc5c3b32679171a2a206a
KuzmiCz85 <95711652+KuzmiCz85@users.noreply.github.com>
2021-12-07 19:53:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+
+            require_once('tridy/Kalkulacka.php');
+        
+            echo ('<h1>Objektová kalkulačka</h1>');
+            echo ('
+                <form method="post">
+                    1.číslo: <br>
+                    <input type="text" name="cislo1"><br>
+                    2.číslo: <br>
+                    <input type="text" name="cislo2"><br><br>
+                    <input type="submit" value="Vypočítat">
+                </form>
+                ');
+            
+            echo('<h2>Výsledky:</h2>');
+            
+            if ($_POST)
+            {
+                if ($_POST['cislo1'] !== "" && $_POST['cislo2'] !== "")
+                {
+                    
+                    $moje_kalkulacka = new Kalkulacka($_POST['cislo1'], $_POST['cislo2']);        
+
+                    echo('Součet: ' . $moje_kalkulacka->soucet() . '<br>');
+
+                    echo ('Rozdíl: ' . $moje_kalkulacka->rozdil() . '<br>');
+
+                    echo ('Součin: ' . $moje_kalkulacka->soucin() . '<br>');
+
+                    echo ('Podíl: ' . $moje_kalkulacka->podil());
+                    }
+            
+                else
+                {
+                    echo ('Zadejte prosím obě čísla.');
+                    
+                }
+            
+            }
+            
+        ?>
+    </body>
+</html>
